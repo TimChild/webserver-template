2c41b7f1dc8ff88c74ccdd2fd6007b7404cc7795
Tim Child <timjchild@gmail.com>
2025-02-13 20:57:01 -0800

    update new-static-site script

--- scripts/setup-webserver-new-static-site.sh
+++ scripts/setup-webserver-new-static-site.sh
@@ -25,28 +25,37 @@ set -e
 
 ssh_name=$1
 site_name=$2
+domain=$3
 
 
 if [ -z "$ssh_name" ] || [ -z "$site_name" ]; then
-    echo "Usage: webserver-init-new-static-site.sh <ssh_name> <site_name>"
+    echo "Usage: webserver-init-new-static-site.sh <ssh_name> <site_name> <domain>"
     exit 1
 fi
 
-heading "Initializing new static site ($site_name) on ($ssh_name)"
+heading "Initializing new static site ($site_name) on ($ssh_name) with domain ($domain)"
 
-# Check that the site does not already exist on the webserver
+# Check that the site does not already exist on the webserver (where it is actually served)
 if ssh $ssh_name "[ -d /srv/www/$site_name ]"; then
     warning "Site $site_name already exists on the webserver"
     exit 1
 fi
 
+if [ -z "$domain" ]; then
+    echo "No domain provided. No Caddy config will be created"
+else
+    echo "Creating caddy config locally..."
+    DOMAIN=$domain SITE_NAME=$site_name \
+        envsubst < caddyfile-templates/template-static.caddy > sites-enabled/$site_name.caddy
+    echo "Sending config to webserver..."
+    scp sites-enabled/$site_name.caddy $ssh_name:~/sites-enabled/
+fi
+
 echo "Creating directories on the webserver..."
-# Create directories on the webserver
 ssh $ssh_name "rm -rf ~/sites/$site_name && mkdir -p ~/sites/$site_name/static"
 ssh $ssh_name "sudo mkdir -p /srv/www/$site_name"
 
-heading "Static site initialized successfully"
+heading "New static site initialized successfully"
+
 echo "Note that site won't be visible until deploy task is run"
 # Deploy task will sync the config (.caddy) as well as static files
-
-
